72051df748250f1effce8fceb084951227307c91
ishwariyer <ishwar.siyer@gmail.com>
2020-08-30 15:03:56 +0530

    Add 2d project

---
+++ 2d/Assets/2DGamekit/Scripts/DoorClose.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorClose : MonoBehaviour
+{
+    public Animator animator = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log( " Player entered " );
+
+        animator.GetComponent<Gamekit2D.HubDoor>().enabled = false;
+
+        animator.Play ("DoorClosing");
+    }
+}
